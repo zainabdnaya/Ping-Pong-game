101d2b8fb92c44f568751ebc58a40a7b4237077c
zdnaya <zdnaya@student.1337.ma>
2022-02-01 16:12:38 +0100

    binary_calculator

--- Binary_Calculator/binaryCalculator.js
+++ Binary_Calculator/binaryCalculator.js
@@ -13,14 +13,6 @@ let btnDiv = document.getElementById("btnDiv");
 
 //this function to khnow if any of operations are vlivked
 
-
-function op_clicked() {
-    if (document.getElementById('btnSum').clicked == true || document.getElementById('btnSub').clicked == true || document.getElementById('btnMul').clicked == true || document.getElementById('btnDiv').clicked == true)
-        return (true);
-    else
-        false
-}
-
 function witch_op() {
     if (document.getElementById('btnSum').clicked == true)
         return ('+');
@@ -34,34 +26,44 @@ function witch_op() {
         return ('/');
 }
 
-let num1 = 0;
-let num2 = 0;
-let tmp;
-let op = false;
+var num1 = "";
+var num2 = "";
+var tmp;
+var op = false;
 
 btn0.addEventListener('click', event => {
-    if (op == false)
-        console.log(" you press a zero  num1");
+    if (op == false) {
+        num1 += "0";
+
+    }
     else
-        console.log("num2");
+        num2 += "0";
 }
 );
 
 btn1.addEventListener('click', event => {
-    console.log(" you press a one");
+    if (op == false) {
+        num1 += "1";
+    }
+    else {
+        num2 += "1";
+    }
 }
 );
 
+var char_op;
 
 btnSum.addEventListener('click', event => {
     console.log(" you press a plus");
     op = true;
+    char_op = '+';
 }
 );
 
 btnSub.addEventListener('click', event => {
     console.log(" you press a sub");
     op = true;
+    char_op = '-';
 
 }
 );
@@ -69,6 +71,7 @@ btnSub.addEventListener('click', event => {
 btnMul.addEventListener('click', event => {
     console.log(" you press a multiplication");
     op = true;
+    char_op = '*';
 
 }
 );
@@ -77,19 +80,54 @@ btnMul.addEventListener('click', event => {
 btnDiv.addEventListener('click', event => {
     console.log(" you press a div");
     op = true;
+    char_op = '/';
 
 }
 );
 
 
 
+var reslt="";
 btnEql.addEventListener('click', event => {
     console.log(" you press a eql");
+    if (op == true) {
+        if (char_op == '+') {
+            reslt = parseInt(num1, 2) + parseInt(num2, 2);
+
+            return (reslt.toString(2));
+        }
+        else if (char_op == '-') {
+            reslt = parseInt(num1, 2) - parseInt(num2, 2);
+            console.log(  "==>" + parseInt(reslt));
+            if (parseInt(reslt) < 0)
+                return ((parseInt(reslt)    >>> 0).toString(2));
+            else
+                return (reslt.toString(2));
+        }
+        else if (char_op == '*') {
+            reslt = parseInt(num1, 2) * parseInt(num2, 2);
+
+            return (reslt.toString(2));
+        }
+        else if (char_op == '/') {
+            reslt = parseInt(num1, 2) / parseInt(num2, 2);
+            return (reslt.toString(2));
+        }
+    }
+    else {
+        alert("Wrong syntax :=> a ( -  or + or / or * ) b ")
+    }
 }
 );
 
 
 btnClr.addEventListener('click', event => {
     console.log(" you press a clear");
+    console.log("=> num1 \t" + num1);
+    console.log("=> num2 \t" + num2);
+    console.log("num 1 = " + parseInt(num1, 2));
+    console.log("num 2 = " + parseInt(num2, 2));
+    console.log("result = " + reslt);
+    console.log("result in binary = " + reslt.toString(2));
 }
 );
