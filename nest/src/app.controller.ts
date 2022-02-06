e5c87fcad5205d1aca884a2c1a1e90c726f9fb2f
zdnaya <zdnaya@student.1337.ma>
2022-02-06 19:52:34 +0100

    2022-02-06 19:52:32

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,  Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Header  } from '@nestjs/common';
+import { Query } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  // @Get('docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   if (version && version === '5') {
+  //     return { url: 'https://docs.nestjs.com/v5/' };
+  //   }
+  // }
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
