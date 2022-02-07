30052263dfad8680eecd03abb0574141be63ca00
zdnaya <zdnaya@student.1337.ma>
2022-02-07 19:43:32 +0000

    2022-02-07 19:43:31

--- nest/src/app.gateway.ts
+++ nest/src/app.gateway.ts
@@ -2,7 +2,7 @@ import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessa
 import { Logger } from '@nestjs/common'
 import { Socket , Server } from 'socket.io';
 
-@WebSocketGateway(3080)
+@WebSocketGateway(3080,{cors: true})
 export class AppGateway implements OnGatewayInit, OnGatewayConnection , OnGatewayDisconnect{
 
   @WebSocketServer()  wss: Server;
@@ -41,6 +41,6 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection , OnGatewa
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, text: string): void 
   {
-    this.wss.emit('msgToClient',text);
+    client.broadcast.emit('msgToClient',text);
   }
 }
